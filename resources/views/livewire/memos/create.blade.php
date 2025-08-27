01c2fe394417657608cfbec90c779ec0ffbd548a
fatowl-dev <kingyo.summer.japan@gmail.com>
2025-08-27 09:45:37 +0900

    日本語化成功

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -2,13 +2,13 @@
 use function Livewire\Volt\{state, rules};
 use App\Models\Memo;
 
+state(['title', 'body']);
+
 // バリデーションルールを定義
 rules([
     'title' => 'required|string|max:50',
     'body' => 'required|string|max:2000',
 ]);
-
-state(['title', 'body']);
 
 $store = function () {
     $this->validate(); // バリデーションチェック
