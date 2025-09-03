8ba549fc4c0663548572493bad4e46aef0cc3a65
fatowl-dev <kingyo.summer.japan@gmail.com>
2025-09-03 18:12:39 +0900

    ->name('memos.index')の重複を削除

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Livewire\Volt\Volt;
 
 Volt::route('/', 'memos.index')->name('memos.index');
 // 一覧ページ
-Volt::route('/memos', 'memos.index')->name('memos.index');
+// Volt::route('/memos', 'memos.index')->name('memos.index');
 // 具体的なパスを先に定義
 Volt::route('/memos/create', 'memos.create')->name('memos.create');
 // 動的パラメータを含むルートは最後に定義
